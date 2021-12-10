861046cb0babfd57e0ad3c59505fb9d54ce5b762
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2021-12-10 17:38:52 +0900

    method overriding

--- src/inheritance/VIPCustomer.java
+++ src/inheritance/VIPCustomer.java
@@ -4,10 +4,17 @@ public class VIPCustomer extends Customer {
 	private int agentID;
 	double saleRatio;
 
-	public VIPCustomer() {
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
 		customerGrade = "VIP";
 		bonusRatio = 0.05;
 		saleRatio = 0.1;
+	}
+
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int) (price * saleRatio);
 	}
 
 	public int getAgentID() {
